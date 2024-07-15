54f82eb040adf2133b6ff08b655469a83ee06195
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-07-15 16:12:28 +0530

    Update AutoGraderPreviewTest.java

--- src/test/java/vandy/mooc/functional/AutoGraderPreviewTest.java
+++ src/test/java/vandy/mooc/functional/AutoGraderPreviewTest.java
@@ -12,64 +12,49 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class AutoGraderPreviewTest {
-
     /**
-     * Runs the autograder preview for the provided test classes and saves
-     * the results to a file.
+     * The test in this class provides the same feedback that is provided when
+     * you submit your assignment to the Coursera grader on the assignment
+     * hand-in page.
      */
     @Test
     public void graderPreview() {
         try {
             run(new File("./build/results.txt"),
-                    List.of(
-                            ArrayTests.class,
-                            FleschKincaidGradeLevelCalculatorTests.class,
-                            BardPlayAnalyzerTests.class
-                    ));
-        } catch (Exception e) {
-            throw new RuntimeException("Error running grader preview", e);
+                List.of(
+                    ArrayTests.class,
+                    FleschKincaidGradeLevelCalculatorTests.class,
+                    BardPlayAnalyzerTests.class
+                ));
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Runs the autograder on the provided test classes and saves the results
-     * to the specified file.
-     *
-     * @param resultsFile The file to write the autograder results to.
-     * @param classes     The list of test classes to evaluate.
-     */
-    public static void run(File resultsFile, List<Class<?>> classes) {
-        try {
-            // Create a JUnitEvaluator instance with the list of test classes
-            JUnitEvaluator eval = new JUnitEvaluator(classes);
+    public static void run(File resultsFile, List<Class<?>> classes) throws Exception {
+        JUnitEvaluator eval = new JUnitEvaluator(classes);
+        JUnitEvaluation estimatedScore = eval.evaluate(new ConsoleFormatter());
 
-            // Evaluate the test classes using ConsoleFormatter for output
-            JUnitEvaluation estimatedScore = eval.evaluate(new ConsoleFormatter());
+        System.out.println(WordUtils.wrap("\n\n" +
+                                              "Your assignment HAS NOT BEEN SUBMITTED. The " +
+                                              "following test results " +
+                                              "only estimate your grade.\n\n", 80));
 
-            // Display the estimated score and feedback to the console
-            System.out.println(WordUtils.wrap("\n\n" +
-                    "Your assignment HAS NOT BEEN SUBMITTED. The " +
-                    "following test results " +
-                    "only estimate your grade.\n\n", 80));
+        System.out.println(WordUtils.wrap("Your estimated score is: " +
+                                              estimatedScore.getScore() + "/" +
+                                              estimatedScore.getTotalPoints(), 80));
 
-            System.out.println(WordUtils.wrap("Your estimated score is: " +
-                    estimatedScore.getScore() + "/" +
-                    estimatedScore.getTotalPoints(), 80));
+        System.out.println(WordUtils.wrap(
+            "(This is not your actual grade for this " +
+                "assignment, just an estimate. Your official grade will be " +
+                "calculated after you submit your submission.zip file to Coursera.\n\n",
+            80));
 
-            System.out.println(WordUtils.wrap(
-                    "(This is not your actual grade for this " +
-                            "assignment, just an estimate. Your official grade will be " +
-                            "calculated after you submit your submission.zip file to Coursera.)\n\n",
-                    80));
+        System.out.println(estimatedScore.getFeedback());
 
-            System.out.println(estimatedScore.getFeedback());
-
-            // Write the feedback to the specified results file
-            try (PrintWriter writer = new PrintWriter(resultsFile, StandardCharsets.UTF_8)) {
-                writer.print(estimatedScore.getFeedback());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Error running grader preview", e);
-        }
+        PrintWriter writer = new PrintWriter(resultsFile, StandardCharsets.UTF_8);
+        writer.print(estimatedScore.getFeedback());
+        writer.close();
     }
 }
